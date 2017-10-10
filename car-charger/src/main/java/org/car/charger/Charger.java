fef59fa5b8771d2af84f958fa520677adbb9e2f8
BartMoroz <bartlomiej.moroz@mobica.com>
2017-10-10 10:23:04 +0200

    added callbacks to charger example

--- car-charger/src/main/java/org/car/charger/Charger.java
+++ car-charger/src/main/java/org/car/charger/Charger.java
@@ -1,8 +1,14 @@
 package org.car.charger;
 
 import java.util.Base64;
+
 import java.util.HashMap;
 import java.util.Map;
+
+import org.car.charger.JsonTags;
+import org.json.simple.JSONObject;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.worldpay.innovation.wpwithin.PSPConfig;
 import com.worldpay.innovation.wpwithin.WPWithinGeneralException;
@@ -17,13 +23,30 @@ import com.worldpay.innovation.wpwithin.types.WWServiceDeliveryToken;
 import com.worldpay.innovation.wpwithin.types.WWTotalPriceResponse;
 
 public class Charger {
-	private static WPWithinWrapper wpw;
+	private WPWithinWrapper wpw;
 
-	public static void main(String[] args) {
+	private JSONObject chargerJsonObject;
+
+	public void setChargerJsonObject(JSONObject chargerJsonObject) {
+		this.chargerJsonObject = chargerJsonObject;
+	}
+
+	private void updateFlow(JSONObject obj, JsonTags tag, String msg) {
+		obj.put(tag.getTag(), msg);
+	}
+
+	public JSONObject getChargerJsonObject() {
+		return this.chargerJsonObject;
+	}
+	
+
+	public void run() throws Exception {
 
 		try {
 
-			System.out.println("Car charger example...");
+			JSONObject chargerObj = new JSONObject();
+			updateFlow(chargerObj, JsonTags.FLOW, "Car charger example...");
+			chargerJsonObject = chargerObj;
 
 			String[] splitedPkgName = Charger.class.getPackage().getName().split("\\.");
 			String rpcLogFile = "rpc-within-" + splitedPkgName[splitedPkgName.length - 1] + ".log";
@@ -74,8 +97,10 @@ public class Charger {
 			// pspConfig.put(PSPConfig.SECURE_NET_ID, "8008609");
 
 			wpw.initProducer(pspConfig);
-
+			updateFlow(chargerObj, JsonTags.FLOW, "Broadcasting...");
+			chargerJsonObject = chargerObj;
 			wpw.startServiceBroadcast(0);
+
 
 		} catch (WPWithinGeneralException e) {
 
@@ -83,26 +108,16 @@ public class Charger {
 		}
 	}
 
-	private static EventListener wpWithinEventListener = new EventListener() {
+	private  EventListener wpWithinEventListener = new EventListener() {
 
 		@Override
-		public void onBeginServiceDelivery(int serviceID, int servicePriceID, WWServiceDeliveryToken wwServiceDeliveryToken,
-				int unitsToSupply) throws WPWithinGeneralException {
-
-			try {
-				System.out.printf("Starting service: %s\n", wpw.getDevice().getServices().get(serviceID).getName());
-				System.out.printf("Service to provide %s\n", wpw.getDevice().getServices().get(serviceID).getPrices().get(servicePriceID).getDescription());
-				System.out.printf("UnitsToSupply: %d\n", unitsToSupply);
-				System.out.printf("SDT.Key: %s\n", wwServiceDeliveryToken.getKey());
-				System.out.printf("SDT.Expiry: %s\n", wwServiceDeliveryToken.getExpiry());
-				System.out.printf("SDT.Issued: %s\n", wwServiceDeliveryToken.getIssued());
-				System.out.printf("SDT.Signature: %s\n",
-						Base64.getEncoder().encodeToString(wwServiceDeliveryToken.getSignature()));
-				System.out.printf("SDT.RefundOnExpiry: %b\n", wwServiceDeliveryToken.isRefundOnExpiry());
-			} catch (Exception e) {
-
-				e.printStackTrace();
-			}
+		public void onBeginServiceDelivery(int serviceID, int servicePriceID,
+				WWServiceDeliveryToken wwServiceDeliveryToken, int unitsToSupply) throws WPWithinGeneralException {
+			JSONObject chargerObj = new JSONObject();
+			updateFlow(chargerObj, JsonTags.FLOW, "Starting service: "+wpw.getDevice().getServices().get(serviceID).getName());
+			updateFlow(chargerObj, JsonTags.DESCRIPTION, "Service to provide: "+wpw.getDevice().getServices().get(serviceID).getPrices().get(servicePriceID).getDescription());
+			updateFlow(chargerObj, JsonTags.UNITS, "UnitsToSupply: "+unitsToSupply);
+			setChargerJsonObject(chargerObj);
 		}
 
 		@Override
@@ -129,38 +144,47 @@ public class Charger {
 		@Override
 		public void onMakePaymentEvent(int totalPrice, String orderCurrency, String clientToken,
 				String orderDescription, String uuid) throws WPWithinGeneralException {
-			System.out.println("Total price: "+totalPrice);
-			System.out.println("Order currency: "+orderCurrency);
-			System.out.println("Client token: "+clientToken);
-			System.out.println("Order description: "+orderDescription);
-			System.out.println("UUID: "+uuid);
 
+			JSONObject chargerObj = new JSONObject();
+			updateFlow(chargerObj, JsonTags.FLOW, "Making payment...");
+			updateFlow(chargerObj, JsonTags.DESCRIPTION, "Order description: "+orderDescription);
+			updateFlow(chargerObj, JsonTags.UNITS, "Total price: "+totalPrice+orderCurrency);
+			setChargerJsonObject(chargerObj);
 		}
 
 		@Override
 		public void onErrorEvent(String msg) throws WPWithinGeneralException {
-			// TODO Auto-generated method stub
-			System.out.println("Mesedz: "+msg);
+			
+			JSONObject chargerObj = new JSONObject();
+			updateFlow(chargerObj, JsonTags.FLOW, "Error occurred: "+msg);
+			setChargerJsonObject(chargerObj);
 
 		}
 
 		@Override
 		public void onServiceDiscoveryEvent(String remoteAddr) throws WPWithinGeneralException {
-			System.out.println("Device discovery innitiated from: "+remoteAddr);
+			JSONObject chargerObj = new JSONObject();
+			updateFlow(chargerObj, JsonTags.FLOW, "Client connected from: "+remoteAddr);
+			setChargerJsonObject(chargerObj);
 		}
 
 		@Override
 		public void onServicePricesEvent(String remoteAddr, int serviceId) throws WPWithinGeneralException {
-			System.out.println("User: "+remoteAddr);
-			System.out.println("Service selected: "+wpw.getDevice().getServices().get(serviceId).getName());
+			
+			JSONObject chargerObj = new JSONObject();
+			updateFlow(chargerObj, JsonTags.FLOW, "Service negotiation...1/2"); 
+			updateFlow(chargerObj, JsonTags.DESCRIPTION, "Service selected: "+wpw.getDevice().getServices().get(serviceId).getName());
+			setChargerJsonObject(chargerObj);
 		}
 
 		@Override
 		public void onServiceTotalPriceEvent(String remoteAddr, int serviceId, WWTotalPriceResponse totalPriceResponse)
 				throws WPWithinGeneralException {
-			System.out.println("User: "+remoteAddr);
-			System.out.println("Client ID: "+totalPriceResponse.getClientId());
-			System.out.println("Option selected: "+wpw.getDevice().getServices().get(serviceId).getPrices().get(totalPriceResponse.getPriceId()));
+			
+			JSONObject chargerObj = new JSONObject();
+			updateFlow(chargerObj, JsonTags.FLOW, "Service negotiation...2/2"); 
+			updateFlow(chargerObj, JsonTags.DESCRIPTION, "Option selected: "+wpw.getDevice().getServices().get(serviceId).getPrices().get(totalPriceResponse.getPriceId()));
+			setChargerJsonObject(chargerObj);
 		}
 	};
 
